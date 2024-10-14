19d6230106ebdc178c9f2c1f324dce541a081ca0
jcsalamanca-uam <jcsalamanca@uamv.edu.ni>
2024-10-14 10:01:09 -0600

    Add project files.

---
+++ Sesion17/Modelos/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion17.Modelos
+{
+    public struct Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+
+    }
+}
---
+++ Sesion17/Servicios/productoServicio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sesion17.Modelos;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion17.Servicios
+{
+    internal class productoServicio
+    {
+        private List<Producto> productos = new List<Producto>();
+
+        public void AgregarProducto(Producto producto)
+        {
+            productos.Add(producto);
+        }
+
+        public List<Producto> GetProductos()
+        {
+            return productos;
+        }
+
+        public void EliminarProducto(Producto producto)
+        {
+            productos.Remove(producto);
+
+        }
+    }
+}
